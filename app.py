066d0bc0dfcc691442e689a212c4aab4781ef7fb
Rit-j <84802049+Rit-j@users.noreply.github.com>
2026-03-31 10:36:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,10 @@ load_dotenv()
 
 openrouter_api_key = os.getenv("OPENROUTER_API_KEY")
 
+# fallback for Streamlit Cloud
+if not openrouter_api_key:
+    openrouter_api_key = st.secrets["OPENROUTER_API_KEY"]
+
 openrouter = OpenAI(
     base_url="https://openrouter.ai/api/v1",
     api_key=openrouter_api_key,
